16183b72d9a1df698ea0276a037ab7859181a67d
KarChun0227 <karchun.0227@hotmail.com>
2017-10-17 23:55:16 +0100

    basic interface setup

--- src/App.js
+++ src/App.js
@@ -20,13 +20,75 @@
       }
     }
 
-    class ContactsApp extends React.Component {
+    class Student extends React.Component {
       render() {
           return (
+            <tr >
+              <td>
+                 { this.props.student.name }
+              </td>
+              <td>
+                 {this.props.student.id }
+              </td>
+              <td>
+                 <input type="button" className="btn btn-primary" value="Edit"/>
+              </td>  
+              <td>
+                 <input type="button" className="btn btn-danger" value="Delete"/>
+              </td>                      
+          </tr>
+
+            ) ;
+        }
+    }
+
+    class StudentsApp extends React.Component {
+      render() {
+        var students = api.getAll() ;
+          return (
                 <div>
-                   <h1>Contact List.</h1>
-                   <ContactsTable contacts={this.props.contacts}  />
+                   <h1>Student List.</h1>
+                   <Table students={students}  />
                 </div>
           );
       }
     }
+
+
+  class StudentList extends React.Component {
+      render() {
+         var studentRows =   this.props.students.map(
+                function(c) {
+                     return <Student key={c.id} student={c} />
+                });
+          return (
+              <tbody >
+                  {studentRows}
+                  <studentForm />
+              </tbody>
+            ) ;
+        }
+    }
+
+  class StudentForm extends React.Component {
+      render() {
+        return (
+          <tr>
+            <td>
+            <input type="text" className="form-control" />
+            </td>
+            <td>
+            <input type="text" className="form-control"/>
+            </td>
+            <td>
+            <input type="text" className="form-control" />
+            </td>
+            <td>
+            <input type="button" className="btn btn-primary" value="Add"/>
+            </td>
+          </tr>
+          )
+      }
+    }
+
+  export default StudentsApp;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import StudentsApp from './App';   
+
+ReactDOM.render(
+    <StudentsApp />,
+    document.getElementById('root')
+    );
